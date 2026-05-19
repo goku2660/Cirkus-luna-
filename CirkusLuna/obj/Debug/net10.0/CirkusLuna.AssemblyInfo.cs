14e685c2d7093e395ec64c5e0ec4451c253b43f3
goku2660 <Shah266003@outlook.dk>
2026-05-19 10:39:36 +0200

    første upload

--- CirkusLuna/obj/Debug/net10.0/CirkusLuna.AssemblyInfo.cs
+++ CirkusLuna/obj/Debug/net10.0/CirkusLuna.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CirkusLuna")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62c72dbca6733d1b99cd39bc62e513958df73560")]
 [assembly: System.Reflection.AssemblyProductAttribute("CirkusLuna")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CirkusLuna")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
