a67a5106fcb1616963cbb4661824589db59ad444
goku2660 <Shah266003@outlook.dk>
2026-05-21 12:13:41 +0200

    fixet admin siden

--- CirkusLuna.web/Pages/Admin.cshtml.cs
+++ CirkusLuna.web/Pages/Admin.cshtml.cs
@@ -1,5 +1,4 @@
 using CirkusLuna.Models;
-using CirkusLuna.Repositories;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CirkusLuna.Web.Pages
@@ -12,97 +11,64 @@ namespace CirkusLuna.Web.Pages
 
         public void OnGet()
         {
-            IndlaesShows();
+            ShowData.Initialiser();
+            Shows = ShowData.Shows;
         }
 
         public void OnPostTilfoejShow()
         {
+            ShowData.Initialiser();
             string cityName = Request.Form["CityName"];
             string dateString = Request.Form["Date"];
             DateTime date = DateTime.Parse(dateString);
 
-            IndlaesShows();
-            int nyId = Shows.Count + 1;
-            Shows.Add(new Show(nyId, cityName, date));
-            GemShows();
+            int nyId = ShowData.Shows.Count + 1;
+            ShowData.Shows.Add(new Show(nyId, cityName, date));
 
             BekraeftetTekst = cityName + " er tilføjet!";
             Bekraeftet = true;
+            Shows = ShowData.Shows;
         }
 
         public void OnPostAendreDato()
         {
+            ShowData.Initialiser();
             int showId = int.Parse(Request.Form["ShowId"]);
             string nyDatoString = Request.Form["NyDato"];
             DateTime nyDato = DateTime.Parse(nyDatoString);
 
-            IndlaesShows();
-            for (int i = 0; i < Shows.Count; i++)
+            for (int i = 0; i < ShowData.Shows.Count; i++)
             {
-                if (Shows[i].ShowId == showId)
+                if (ShowData.Shows[i].ShowId == showId)
                 {
-                    Shows[i].Date = nyDato;
-                    BekraeftetTekst = Shows[i].CityName + " er opdateret!";
+                    ShowData.Shows[i].Date = nyDato;
+                    BekraeftetTekst = ShowData.Shows[i].CityName + " er opdateret!";
                 }
             }
-            GemShows();
+
             Bekraeftet = true;
+            Shows = ShowData.Shows;
         }
 
         public void OnPostTilfoejArtist()
         {
+            ShowData.Initialiser();
             int showId = int.Parse(Request.Form["ShowId"]);
             string navn = Request.Form["ArtistNavn"];
             string specialitet = Request.Form["Specialitet"];
 
-            IndlaesShows();
-            for (int i = 0; i < Shows.Count; i++)
+            for (int i = 0; i < ShowData.Shows.Count; i++)
             {
-                if (Shows[i].ShowId == showId)
+                if (ShowData.Shows[i].ShowId == showId)
                 {
                     Artist artist = new Artist(i + 1, navn, "", specialitet);
-                    Shows[i].AddArtist(artist);
-                    BekraeftetTekst = navn + " er tilføjet til " + Shows[i].CityName;
+                    ShowData.Shows[i].AddArtist(artist);
+                    BekraeftetTekst = navn + " er tilføjet til " + ShowData.Shows[i].CityName;
                 }
             }
-            GemShows();
+
             Bekraeftet = true;
-        }
-
-        private void IndlaesShows()
-        {
-            string gemteShows = TempData.Peek("Shows") as string;
-            if (gemteShows != null && gemteShows != "")
-            {
-                string[] showArray = gemteShows.Split(';');
-                for (int i = 0; i < showArray.Length; i++)
-                {
-                    string[] dele = showArray[i].Split(',');
-                    Show show = new Show(int.Parse(dele[0]), dele[1], DateTime.Parse(dele[2]));
-                    Shows.Add(show);
-                }
-            }
-            else
-            {
-                Shows.Add(new Show(1, "Copenhagen", DateTime.Today.AddDays(10)));
-                Shows.Add(new Show(2, "Aarhus", DateTime.Today.AddDays(20)));
-                Shows.Add(new Show(3, "Odense", DateTime.Today.AddDays(30)));
-                GemShows();
-            }
-        }
-
-        private void GemShows()
-        {
-            string gemteShows = "";
-            for (int i = 0; i < Shows.Count; i++)
-            {
-                gemteShows += Shows[i].ShowId + "," + Shows[i].CityName + "," + Shows[i].Date.ToString("yyyy-MM-dd");
-                if (i < Shows.Count - 1)
-                {
-                    gemteShows += ";";
-                }
-            }
-            TempData["Shows"] = gemteShows;
+            Shows = ShowData.Shows;
         }
     }
 }
